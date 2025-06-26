91d5f6dbc48a9e002d1750b579eec617940d6298
Peter Ayeni <peter.ayeni@netacea.com>
2025-06-26 20:41:43 +0100

    configure biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import HcpControlPanel from "@/components/hcp-control-panel"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+import HcpControlPanel from "@/components/hcp-control-panel";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Page() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <HcpControlPanel/>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  )
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+					<HcpControlPanel />
+				</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
 }
