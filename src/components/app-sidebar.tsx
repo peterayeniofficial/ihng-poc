6a585b036b5b654a4cc339bc3f3d1ee351b7ac04
Peter Ayeni <peter.ayeni@netacea.com>
2025-06-26 18:07:09 +0100

    complete main dashboard layout

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -29,8 +29,8 @@ import {
 // This is sample data.
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "Emilly Carter",
+    email: "ec@example.com",
     avatar: "/avatars/shadcn.jpg",
   },
   teams: [
